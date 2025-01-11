02c34d1a8ef885055c575bf19a6bf8da7f1d32bb
soniya malviya <soniya.04malviya@gmail.com>
2025-01-12 05:05:35 +0530

    experince added

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -18,7 +18,7 @@ const content = [
   {
     title: "Fullstack Intern at IIT Roorkee",
     description:
-      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      "I served as a Fullstack Intern at IIT Roorkee, where I contributed to the development and enhancement of web applications. During my internship, I worked on both frontend and backend development, utilizing technologies such as React.js, Node.js, and various databases. My role involved designing user interfaces, implementing APIs, and ensuring the seamless integration of different components of the applications. This experience allowed me to strengthen my skills in full-stack development and gain valuable exposure to real-world projects in a prestigious academic environment.",
     content: (
       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
         <Image
@@ -36,7 +36,7 @@ const content = [
   {
     title: "Mentor and Open Source Contributor at GSSOC",
     description:
-      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      "I have served as a Mentor and Open Source Contributor at GirlScript Summer of Code (GSSoC), where I guided participants in developing and improving open-source projects. As a mentor, I provided technical support, reviewed code, and shared best practices to help contributors enhance their skills in software development. My role also involved contributing to various projects by writing code, fixing bugs, and adding new features. This experience allowed me to foster a collaborative learning environment, promote open-source contributions, and refine my mentoring and technical skills.",
     content: (
       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--black),var(--black))] flex items-center justify-center text-white">
         <Image
